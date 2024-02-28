0b926c5f5c39c8500c683c907a7aef2cb8dc1afd
Nikhil Vishwakarma <92681220+Nikhil-029@users.noreply.github.com>
2024-02-28 22:13:53 +0530

    Virtual Mouse

---
+++ HandTracking.py
@@ -0,0 +1,36 @@
+import mediapipe as mp
+import time
+import cv2 as cv
+
+cam = cv.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    # cv.namedWindow("cam-test")
+    s, img = cam.read()
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        # for each hand landmarks
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                if id ==0:
+                    cv.circle(img, (cx,cy),25, (255,0,255), cv.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.imshow("Image", img)
+    cv.waitKey(1)
+
